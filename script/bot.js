e54fdeabbb5cf71a507309b2af1c7d9cddd54a08
Adilson Bacelar <a.m.bacelar@gmail.com>
2023-11-05 18:18:59 +0000

    initial commit

---
+++ script/bot.js
@@ -0,0 +1,123 @@
+// bot fonctionnement
+const bot = function () {
+  const efitterChat = document.getElementById('efitter-chat');
+  const container = document.getElementById('efitter-chat-container');
+  const inner = document.getElementById('efitter-chat-inner');
+  let restartButton = null;
+
+  const sleep = function (ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
+
+  const scrollContainer = function () {
+    inner.scrollTop = inner.scrollHeight;
+  };
+
+  const insertNewChatItem = function (elem) {
+    efitterChat.appendChild(elem);
+    scrollContainer();
+    elem.classList.add('activated');
+  };
+
+  const printResponse = async function (step) {
+    const response = document.createElement('div');
+    response.classList.add('chat-response');
+    response.innerHTML = step.text;
+    insertNewChatItem(response);
+    // TODO: create a loading phase
+    await sleep(1500);
+
+    if (step.options) {
+      const choices = document.createElement('div');
+      choices.classList.add('choices');
+      step.options.forEach(function (option) {
+        const button = document.createElement(option.url ? 'a' : 'button');
+        button.classList.add('choice');
+        button.innerHTML = option.text;
+        if (option.url) {
+          button.href = option.url;
+        } else {
+          button.dataset.next = option.next;
+        }
+        choices.appendChild(button);
+      });
+      insertNewChatItem(choices);
+    } else if (step.next) {
+      printResponse(chat[step.next]);
+    }
+  };
+
+  const printChoice = function (choice) {
+    const choiceElem = document.createElement('div');
+    choiceElem.classList.add('chat-ask');
+    choiceElem.innerHTML = choice.innerHTML;
+    insertNewChatItem(choiceElem);
+  };
+
+  const disableAllChoices = function () {
+    const choices = document.querySelectorAll('.choice');
+    choices.forEach(function (choice) {
+      choice.disabled = 'disabled';
+    });
+  };
+
+  const handleChoice = async function (e) {
+    if (!e.target.classList.contains('choice') || e.target.tagName === 'A') {
+      const button = e.target.closest('#efitter-chat-container .choice');
+
+      if (button !== null) {
+        button.click();
+      }
+
+      return;
+    }
+    e.preventDefault();
+    const choice = e.target;
+
+    disableAllChoices();
+
+    printChoice(choice);
+    scrollContainer();
+
+    await sleep(1500);
+
+    if (choice.dataset.next) {
+      printResponse(chat[choice.dataset.next]);
+    }
+  };
+
+  const startConversation = function () {
+    printResponse(chat[1]);
+  };
+
+  const handleRestart = function () {
+    startConversation();
+  };
+
+  const init = function () {
+    container.addEventListener('click', handleChoice);
+    restartButton = document.createElement('button');
+    restartButton.innerText = 'restart';
+    restartButton.classList.add('restart');
+    restartButton.addEventListener('click', handleRestart);
+    container.appendChild(restartButton);
+    startConversation();
+  };
+  init();
+};
+
+bot();
+
+document.getElementById('chatbot_toggle').onclick = function () {
+  if (document.getElementById('chatbot').classList.contains('collapsed')) {
+    document.getElementById('chatbot').classList.remove('collapsed');
+    document.getElementById('chatbot_toggle').children[0].style.display =
+      'none';
+    document.getElementById('chatbot_toggle').children[1].style.display = '';
+  } else {
+    document.getElementById('chatbot').classList.add('collapsed');
+    document.getElementById('chatbot_toggle').children[0].style.display = '';
+    document.getElementById('chatbot_toggle').children[1].style.display =
+      'none';
+  }
+};
---
+++ script/chat.js
@@ -0,0 +1,71 @@
+// crée la conversation
+const chat = {
+  1: {
+    text: "Hey! 👋  I'm efitter. I'm here to help you shop 😁",
+    next: 2,
+  },
+  2: {
+    text: 'How can I help you?',
+    options: [
+      {
+        text: 'Which size is best for me?',
+        next: 3,
+      },
+      {
+        text: 'What is the material like?',
+        next: 4,
+      },
+    ],
+  },
+  3: {
+    text: 'Based on your shopping history, size {{size}} normally works for you...',
+    next: 7,
+  },
+  4: {
+    text: '{{material}}',
+    options: [
+      {
+        text: 'Great!',
+        next: 5,
+      },
+      {
+        text: 'Which size is best for me?',
+        next: 7,
+      },
+    ],
+  },
+  5: {
+    text: "Fab! Don't forget to add your item to your basket 💃",
+  },
+  6: {},
+  7: {
+    text: 'Size {{size}} should fit you fine!',
+    next: 8,
+  },
+  8: {
+    text: 'Does this prediction seem right to you?',
+    options: [
+      {
+        text: "It's spot on!",
+        next: 9,
+      },
+      {
+        text: "Hmm I'm not sure",
+        next: 10,
+      },
+    ],
+  },
+  9: {
+    text: `Great! 💃<br/><br/>To make efitter even better, please fill out this survey (we'll email you in a few days in case you want to wait for your items to arrive) 😊`,
+    options: [
+      {
+        text: "What's the material like?",
+        next: 4,
+      },
+    ],
+  },
+  10: {
+    text: 'No worries. efitter gets smarter over time so please fill out this survey to make it even better 😊',
+    next: 11,
+  },
+};
